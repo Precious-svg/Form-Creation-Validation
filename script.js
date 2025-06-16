7f6a55a34b785815ced8ad33693414a74a9e8b72
Precious Ogwezhi <pogwezhi@gmail.com>
2025-06-16 14:34:31 +0100

    initial commit

---
+++ script.js
@@ -0,0 +1,40 @@
+document.addEventListener('DOMContentLoaded', () => {
+
+    const form = document.getElementById("registration-form")
+
+    const feedbackDiv = document.getElementById('form-feedback');
+
+    form.addEventListener('submit', () => {
+        e.preventDefault();
+
+        const username = document.getElementById('username').value.trim();
+        const email = document.getElementById('email').value.trim();
+        const password =  document.getElementById('password').value.trim();
+
+       let isValid = true;
+       let messages = []
+
+       if(username.length < 3){
+         isValid = false;
+
+          messages.push("Username is too short")
+        };
+
+        if(!email.includes('@') || !email.includes(',')){
+          isValid = false;
+          messages.push('Email must include both "@" and "." characters.')
+       };
+
+       feedbackDiv.style.display = 'block';
+       if(isValid){
+          feedbackDiv.textContent('"Registration Successful!')
+          feedbackDiv.style.color = "#28a745";
+        } else{
+           feedbackDiv.innerHTML = messages.join('<br>')
+           feedbackDiv.style.color = "#dc354";
+      }
+    })
+})
+
+   
+
